fade8c7bda029e6fe7cce4fe0269861d1cc3f947
Abn Alsrag <alsragbode@gmail.com>
2023-02-05 15:44:49 +0200

    The fucking start again

--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -1,6 +1,7 @@
 #include "Application.h"
 
-#include <SDL2\SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 
 namespace NAQH{
@@ -20,17 +21,32 @@ namespace NAQH{
 		SDL_SetWindowResizable(window, SDL_TRUE);
         SDL_Event event;
         bool running = true;
+        SDL_FRect rect = {100, 100, 200, 200};
 
         while(running)
         {
             while(SDL_PollEvent(&event))
             {
-                if (event.type == SDL_QUIT)
+                if(event.type == SDL_QUIT)
                     running = false;
+
+                if(event.type == SDL_KEYDOWN)
+                {
+                    if(event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+                        rect.x -= 2;
+                    if(event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+                        rect.x += 2;
+                    if(event.key.keysym.scancode == SDL_SCANCODE_UP)
+                        rect.y -= 2;
+                    if(event.key.keysym.scancode == SDL_SCANCODE_DOWN)
+                        rect.y += 2;
+                }
             }
 
             SDL_SetRenderDrawColor(renderer, 17.85, 33.15, 43.35, 255);
             SDL_RenderClear(renderer);
+            SDL_Texture* img = IMG_LoadTexture(renderer, "res/ScreenShot/screenshot.bmp");
+            SDL_RenderCopyF(renderer, img, NULL, &rect);
             SDL_RenderPresent(renderer);
         }
 
